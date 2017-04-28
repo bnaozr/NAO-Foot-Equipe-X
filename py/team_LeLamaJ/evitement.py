5e62f362dff794ea928fc16c6054791b4a7d895e
Elodie NOELE <elodie.noele@ensta-bretagne.org>
2017-04-28 17:11:32 +0200

    Ajout de fichiers python naocmd

--- py/team_LeLamaJ/evitement.py
+++ py/team_LeLamaJ/evitement.py
@@ -44,14 +44,14 @@ def eviter():
     #        motionProxy.moveTo (x, y, theta)
     #        sonarL = memoryProxy.getData("Device/SubDeviceList/US/Left/Sensor/Value")
     #        sonarR = memoryProxy.getData("Device/SubDeviceList/US/Right/Sensor/Value")
-        if sonarL <= 0.5 and sonarL>0.19:
+        if sonarL <= 0.5 and sonarL>0.3:
             x = 0.2
             y = 0.0
             theta = -math.pi/1.5
             motionProxy.moveTo (x, y, theta)
             sonarL = memoryProxy.getData("Device/SubDeviceList/US/Left/Sensor/Value")
             sonarR = memoryProxy.getData("Device/SubDeviceList/US/Right/Sensor/Value")
-        elif sonarR <= 0.5 and sonarR>0.19:
+        elif sonarR <= 0.5 and sonarR>0.3:
             x = 0.2
             y = 0.0
             theta = math.pi/1.5
@@ -59,17 +59,19 @@ def eviter():
             sonarL = memoryProxy.getData("Device/SubDeviceList/US/Left/Sensor/Value")
             sonarR = memoryProxy.getData("Device/SubDeviceList/US/Right/Sensor/Value")
         
-        if (sonarL<0.19  or sonarR<0.19) and (sonarL>0.1 or sonarR>0.1) :
+        if (sonarL<=0.3  or sonarR<=0.3) and (sonarL>0.2 or sonarR>0.2) :
             motionProxy.setWalkArmsEnabled(True, True)
             motionProxy.moveTo(-0.2,0,0)
             sonarL = memoryProxy.getData("Device/SubDeviceList/US/Left/Sensor/Value")
             sonarR = memoryProxy.getData("Device/SubDeviceList/US/Right/Sensor/Value")
-        elif sonarL<=0.1 or sonarR<=0.1:
-            if sonarL<0.1 :
+      
+            
+        elif sonarL<=0.2 or sonarR<=0.2:
+            if sonarL<0.2 :
                 motionProxy.moveTo(-0.2,0,-(math.pi)/4)
                 sonarL = memoryProxy.getData("Device/SubDeviceList/US/Left/Sensor/Value")
                 sonarR = memoryProxy.getData("Device/SubDeviceList/US/Right/Sensor/Value")
-            elif sonarR<0.1: 
+            elif sonarR<0.2: 
                 motionProxy.moveTo(-0.2,0,math.pi/4)
                 sonarL = memoryProxy.getData("Device/SubDeviceList/US/Left/Sensor/Value")
                 sonarR = memoryProxy.getData("Device/SubDeviceList/US/Right/Sensor/Value")
